6c9754a7cefbcee93b48be6212291bed7f1f03fd
Bruno Matheus <bruno.mat@live.com>
2023-02-21 16:35:01 -0300

    Values and varibles class - 10

---
+++ 01-Fundamentals-Part-1/starter/README.md
@@ -0,0 +1,3 @@
+* Varibles only can have letters, numbers, underscore and dollar sign.
+* Don't use "name" as a verible
+* Use upper case to name constant that never changes like "PI = 3.1415"
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,2 +1,25 @@
-console.log('Working on a forked directory')
-//Loaded the complete folder
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;  // Use upper case for constants that never changes
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "Programmer";  //Do not use varibles name like that
+let job2 = "Teacher";
+
+console.log(myFirstJob);
