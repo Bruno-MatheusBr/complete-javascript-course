1b8507cd28b3d0d14b3b4f27b4fdb9552b598404
Bruno Matheus <bruno.mat@live.com>
2023-02-22 09:29:02 -0300

    13 . let, const and var

--- 01-Fundamentals-Part-1/starter/README.md
+++ 01-Fundamentals-Part-1/starter/README.md
@@ -1,3 +1,7 @@
 * Varibles only can have letters, numbers, underscore and dollar sign.
 * Don't use "name" as a verible
 * Use upper case to name constant that never changes like "PI = 3.1415"
+* Const cannot be redeclared
+* Const cannot be set undefined. It always has to set a value
+* Var works some way like Let. It can be redeclared
+* Declaring a varible is not mandatory, but never do that.
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -25,4 +25,40 @@ let job1 = "Programmer";  //Do not use varibles name like that
 let job2 = "Teacher";
 
 console.log(myFirstJob);
+
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+let year;
+
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990  // It will return an error - redeclaring a const varible
+// const job; // Const cannot be set undefined. It will return an error
+
+var job = 'programmer';
+job = 'teacher';  // Var works some way like Let. It can be redeclared
+
+lastName = 'Domingues'; // Declaring a varible is not mandatory, but never do that.
+console.log(lastName);
+console.log(typeof lastName);
 */
+
+// ##### 14. BASIC OPERATORS #####
+
