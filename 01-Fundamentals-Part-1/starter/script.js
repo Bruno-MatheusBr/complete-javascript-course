8e22179a2b392b7399de824fdecc1402b67a170e
Bruno Matheus <bruno.mat@live.com>
2023-02-24 16:40:28 -0300

    26. The Switch Statement

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -296,3 +296,51 @@ console.log(n);
 // } else {
 //     console.log('No one socred more than 100 point. Both loose.');
 // }
+// ###############################################################################
+
+// ##### 26. The Switch Statement #####
+
+// const day = prompt('Choose a day!');
+
+// switch (day) {
+//     case 'monday': //day === 'monday'
+//         console.log('Plan course structure.');
+//         console.log('Go to coding meetup.');
+//         break;
+//     case 'tuesday':
+//         console.log('Prepare theory videos.');
+//         break;
+//     case 'wednesday':
+//     case 'thursday':
+//         console.log('Write code exemples.');
+//         break;
+//     case 'friday':
+//         console.log('Record videos.');
+//         break;
+//     case 'saturday':
+//     case 'sunday':
+//         console.log('Enjoy the weekend ❤️🎉');
+//         break;
+//     default:
+//         console.log('Not a valid day.');
+// }
+
+// console.log('-----------------')
+
+// if (day === 'monday') {
+//     console.log('Plan course structure.');
+//     console.log('Go to coding meetup.');
+// } else if (day === 'tuesday') {
+//     console.log('Prepare theory videos.');
+// } else if (day === 'wednesday' || day === 'thursday') {
+//     console.log('Write code exemples.');
+// } else if (day === 'friday') {
+//     console.log('Record videos.');
+// } else if ( day === 'saturday' || day === 'sunday') {
+//     console.log('Enjoy the weekend ❤️🎉');
+// } else {
+//     console.log('Not a valid day.');
+// }
+
+// ##### 27. Statements and Expressions #####
+
