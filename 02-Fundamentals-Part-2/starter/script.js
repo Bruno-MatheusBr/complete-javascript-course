5424b32155fdd5809e503c457799d2cb8f9e5647
Bruno Matheus <bruno.mat@live.com>
2023-02-25 09:06:39 -0300

    32. Strict Mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// ###### 32. Using Strict Mode ######
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive! :D');
+
+// const interface = 'Audio'; // Strict mode don't allow reserved words from JS
+// const private = 543;
+
+//  ###### 33. Functions ######
+
