6a398fef8c68ba519935edf5128c0f1eb74f2ef3
Bruno Matheus <bruno.mat@live.com>
2023-02-25 09:42:35 -0300

    33. Functions - part 01

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -2,14 +2,32 @@
 
 // ###### 32. Using Strict Mode ######
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log('I can drive! :D');
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log('I can drive! :D');
 
 // const interface = 'Audio'; // Strict mode don't allow reserved words from JS
 // const private = 543;
 
 //  ###### 33. Functions ######
+
+function logger() {
+    console.log('My name is Bruno!');
+}
+
+// calling / running / invoking the function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples,oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0); // To get the function return, it needs to storage the parameters in a varible
+console.log(appleJuice); // Than call the varible to get the function return
 
