b5f0e599e687e136c07ddc5dbb7395281d44a5c6
Bruno Matheus <bruno.mat@live.com>
2023-02-26 22:50:18 -0300

    33. Functions - Final

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -23,11 +23,14 @@ logger();
 logger();
 
 function fruitProcessor(apples, oranges) {
-    console.log(apples,oranges);
+    // console.log(apples,oranges);
     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
     return juice;
 }
 
 const appleJuice = fruitProcessor(5, 0); // To get the function return, it needs to storage the parameters in a varible
 console.log(appleJuice); // Than call the varible to get the function return
+console.log(fruitProcessor(5, 0)); // Or just use console.log without storage in a varible
 
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
