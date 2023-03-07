86be567b8d48940c2aff2c2e4d051693d2dfff61
Bruno Matheus <bruno.mat@live.com>
2023-03-07 15:12:27 -0300

    50. CODING CHALLENGE 4 - FINAL

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -442,3 +442,30 @@
 
 // ############ 50. CODING CHALLENGE 4 ############
 
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+function calcTip(bill) {
+    const tip = bill > 50 && bill < 300 ? bill * 0.15 : bill * 0.2;
+    return tip;
+}
+
+for (let i = 0; i < bills.length; i++) {
+    tips.push(calcTip(bills[i]));
+    totals.push(bills[i] + tips[i]);
+}
+console.log(bills, tips, totals);
+
+function calcAverage(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum = sum + arr[i];
+    }
+    return sum / arr.length;
+}
+
+console.log(calcAverage([2, 4, 6]));
+console.log(calcAverage(totals));
+
+// ############ FINAL ############
