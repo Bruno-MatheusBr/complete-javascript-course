4e8effc1a396620f4cb8a92f188fd859dea8da77
Bruno Matheus <bruno.mat@live.com>
2023-03-08 17:27:21 -0300

    Class 60

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 // ########## 72. Selection and Manipulating Elements ##########
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉 Correct Number!';
@@ -10,3 +11,8 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 25;
 console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+});
