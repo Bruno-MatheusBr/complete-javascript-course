0515381dec2aff6ebbaf7bdc3d0a32f3d6ae0c38
Bruno Matheus <bruno.mat@live.com>
2023-03-13 18:15:51 -0300

    82. Project 3: Pig Game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const dice = document.querySelector('.dice');
+
+dice.classList.add('hidden');
+score0El.textContent = 0;
+score1El.textContent = 0;
