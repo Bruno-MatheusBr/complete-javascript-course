beb7bc2be45c23a47013aad32fb7516d6d882b44
Bruno Matheus <bruno.mat@live.com>
2023-03-14 10:19:45 -0300

    83. Rolling Dice: Working in Current Score Pl 0

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -3,8 +3,27 @@
 // Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
-const dice = document.querySelector('.dice');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1')
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new')
+const btnRoll = document.querySelector('.btn--roll')
+const btnHold = document.querySelector('.btn--hold')
 
-dice.classList.add('hidden');
+// Starting Conditions
+diceEl.classList.add('hidden');
 score0El.textContent = 0;
 score1El.textContent = 0;
+
+// Roling dice functionality
+btnRoll.addEventListener('click', function () {
+    // 1. Generate the random dice number
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    // 2. Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    // 3. Checking for rolls rules
+
+})
