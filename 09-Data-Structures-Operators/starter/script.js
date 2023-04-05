b1815f122a0db2007bbc729b38c6d1416f7806eb
Bruno Matheus <bruno.mat@live.com>
2023-04-05 16:25:00 -0300

    112. Enhanced Objects Literals

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -5,33 +5,37 @@ const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
+
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 12 + 12,
+  },
+};
+console.log(openingHours);
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours,
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
   // Destructuring Arrays Real Wolrd Exemples
-  order: function (starterIndex, mainIndex) {
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
   },
   // Destructuring Objects Real World Exemple
-  orderDelivery: function ({ address, time, starterIndex, mainIndex }) {
+  orderDelivery({ address, time, starterIndex, mainIndex }) {
     console.log(`Order: ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]}. Address ${address} at ${time}.`);
   },
   // Using Spread Operator in Real World Exemple
@@ -44,7 +48,7 @@ const restaurant = {
   },
 };
 // /////////////// 112. Enhanced Object Literals ///////////////
-
+// Class writen at the begining of the sheet
 
 /*
 // /////////////// 111. lOOPING ARRAYS: THE FOR-OF LOOP ///////////////
