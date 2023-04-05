632485fbdc17d13138e97581a351bf70ec98ab2e
Bruno Matheus <bruno.mat@live.com>
2023-04-05 11:00:42 -0300

    110. Coding Challenge 01

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -43,6 +43,90 @@ const restaurant = {
     console.log(otherIng);
   },
 };
+
+/*
+// /////////////// 110. CODING CHALLLENGE 01 ///////////////
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+    'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 11.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+// 1.
+const [players1, players2] = game.players;
+console.log(players1);
+console.log(players2);
+
+// 2. 
+const [gk, ...fieldPlayers] = game.players[0];
+console.log(gk);
+console.log(fieldPlayers);
+
+// 3.
+const allPlayers = [...game.players[0], ...game.players[1]];
+console.log(allPlayers);
+
+// 4.
+const players1Final = ['Thiago', 'Coutinho', 'Perisic', ...game.players[0]];
+console.log(players1Final);
+
+// 5.
+const { team1, x: draw, team2 } = game.odds;
+console.log(team1);
+console.log(draw);
+console.log(team2);
+
+// 6.
+const printGoals = function (...player) {
+  console.log(player);
+  console.log(`${player.length} goals was scored.`);
+};
+
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals(...game.scored);
+
+// 7. 
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 2 is more likely to win');
+*/
+
+
 /*
 // /////////////// 109. LOGICAL ASSIGNMENT OPERATORS ///////////////
 
